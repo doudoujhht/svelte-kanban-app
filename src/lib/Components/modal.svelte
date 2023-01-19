c383559cfcb0204aa18131c7aeac6d8293274d08
doudoujhht <100162576+doudoujhht@users.noreply.github.com>
2023-01-19 19:42:17 +0000

    modal darkmode add

--- src/lib/Components/modal.svelte
+++ src/lib/Components/modal.svelte
@@ -1,6 +1,8 @@
-<div class="h-screen w-screen fixed top-0 left-0 flex justify-center items-center shadow-none bg-[#000] bg-opacity-50"
-     on:click|self>
-    <div class="bg-white w-[30%] shadow-none">
-        <slot/>
-    </div>
+<div
+	class="h-screen w-screen fixed top-0 left-0 flex justify-center items-center shadow-none bg-[#000] bg-opacity-50"
+	on:click|self
+>
+	<div class="bg-white dark»:bg-black w-[30%] shadow-none">
+		<slot />
+	</div>
 </div>
